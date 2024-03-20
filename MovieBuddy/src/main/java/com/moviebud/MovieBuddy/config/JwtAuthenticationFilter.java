d2bf0d00a605ac051e93cadfda0a1710d2c1150a
RazzzyK <Razenok@yahoo.com>
2024-03-20 07:06:28 -0400

    Hitting secured endpoint now!

--- MovieBuddy/src/main/java/com/moviebud/MovieBuddy/config/JwtAuthenticationFilter.java
+++ MovieBuddy/src/main/java/com/moviebud/MovieBuddy/config/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,7 +21,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
-    private UserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
